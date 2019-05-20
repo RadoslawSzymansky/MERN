eb50f39e21f5f99c154c9655d4e61816595687c6
Radoslaw Szymanski <radek199@gmail.com>
2019-05-20 21:42:04 +0200

    working on client side !

--- server.js
+++ server.js
@@ -1,38 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
-const config = require('config');
+const bodyParser = require('body-parser');
+const path = require('path')
+
+const items = require('./routes/api/items')
+
 const app = express();
 
-// Bodyparser Middleware
-app.use(express.json());
+//Bodyparsse Middleware
+app.use(bodyParser.json());
 
-// DB Config
-const db = config.get("mongoURI");
+//DB config
+const db = require('./config/keys').mongoURI;
+//Connect to MOngoDB
+mongoose.connect(db)
+    .then(() => {
+        console.log("mondoDB connected")
+    })
+    .catch(err => console.log(err))
 
-// Connect to Mongo
-mongoose
-    .connect(db, {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    }) // Adding new mongo url parser
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+// wszystko co z ta sciezka ma isc do api
+app.use('/api/items', items)
 
-// Use Routes
-app.use('/api/items', require('./routes/api/items'));
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-
-// Serve static assets if in production
+/// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
+    //setStatic folder
     app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirnamem, 'client', 'build', 'index.html'))
+    })
 }
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('server started at port: ', port))
